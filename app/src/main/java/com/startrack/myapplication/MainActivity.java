30d2344d81a6afef451d6bf369e937df2aaf09a9
orelr <orelramadi@gmail.com>
2020-10-21 18:03:38 +0300

    1. Scroll view added. 2. RecyclerView added.

--- app/src/main/java/com/startrack/myapplication/MainActivity.java
+++ app/src/main/java/com/startrack/myapplication/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.INotificationSideChannel;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -32,9 +33,9 @@ public class MainActivity extends AppCompatActivity {
 
 
         business1 = new ArrayList<>();
-        business1.add(new Businesses(R.drawable.upneti,"Upneti",getString(R.string.service_description),"0523456789","5","10$"));
+        business1.add(new Businesses(R.drawable.upneti,"Upneti",getString(R.string.service_description),"052-345-6789","5","10$"));
         business1.add(new Businesses(R.mipmap.magicflex_foreground,"MagicFlex",getString(R.string.service_description),"03-444-5555","10","15$"));
-        business1.add(new Businesses(R.drawable.upneti,"Upneti2",getString(R.string.service_description),"077-750-790","15","20$"));
+        business1.add(new Businesses(R.drawable.ic_launcher_foreground,"Company Name", "Description","Phone Number","15","20$"));
 
 
         myrecyclerView = (RecyclerView)findViewById(R.id.RecyclerView);
